a45d9c4095f97a5d3980c3b7cf327a4b755502d7
Prathamesh Patil <81880631+prathamesh25102000@users.noreply.github.com>
2023-03-06 22:30:21 +0530

    Create Bali.js

---
+++ travel_advisor/src/Components/Blog/Bali.js
@@ -0,0 +1,360 @@
+import React from "react";
+import "./blog.css";
+
+const BaliBlog = () => {
+  return (
+    <div className="japanBlog" style={{ paddingTop: "100px" }}>
+      <div>
+        <h1 className="entryTitle">THE 6 BEST HOTELS IN BALI</h1>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/balihostelsmain.jpeg"
+        />
+        <span>
+          <strong>Posted</strong>: 2/5/2023 | February 5th, 2023
+        </span>
+        <p style={{ marginTop: "26px" }}>
+          A hub for hippies, yogis, and digital nomads, Bali is one of the most
+          popular destinations in Southeast Asia. It was popular when I first
+          started going in 2010 and has only increased in popularity. While
+          parts of Bali struggle with overtourism (I’m looking at you, Kuta
+          Beach), there are still massive swaths of the island where you can get
+          away from the crowds and enjoy the picture-perfect landscape.
+        </p>
+        <p>
+          There is a legion of amazing things to see and do on this island — no
+          matter your interests or budget. Hiking, surfing, sunbathing,
+          exploring the unique food scene, taking a cooking class, spotting
+          wildlife, visiting hot springs, and just chilling out are all popular
+          activities in Bali. Choosing a hostel near the things you want to see
+          and do will save you money as well — money you can then spend on
+          another yoga class or diving trip or a round of beers at the bar.
+        </p>
+        <p>
+          But before you start browsing for a hostel, here are four things you
+          need to remember before picking a place to stay:
+        </p>
+        <ol>
+          <li>
+            <text className="listText">Location –</text> Bali is a large island
+            dotted with various towns and villages. So, picking your hostel in a
+            location you want to be based in is essential.
+          </li>
+          <li>
+            <text className="listText"> Price –</text> As in any destination,
+            you get what you pay for. If you pick a really cheap hostel, you’re
+            probably going to get one that is small and cramped and doesn’t
+            offer great service.
+          </li>
+          <li>
+            <text className="listText">Amenities –</text> Every hostel offers
+            free Wi-Fi, and most have self-catering facilities so you can cook
+            your own meals. Many — but not all — include free breakfast, so be
+            sure to do your research to find the one that best meets your needs.
+          </li>
+          <li>
+            <text className="listText">Staff –</text> All the hostels listed
+            here have an amazing staff who are super friendly and knowledgeable.
+            Even if you don’t end up staying at one of the places listed below,
+            be sure to look up reviews so you have staff who are helpful and
+            friendly. They can make or break a hostel!
+          </li>
+        </ol>
+        <p>
+          Below is my list of the best hostels in Bali. If you don’t want to
+          read the longer list below, the following are the best in each
+          category:
+        </p>
+        <strong>
+          Best Hotel for Budget Travelers:{" "}
+          <a
+            href="https://www.hostelworld.com/pwa/hosteldetails.php/Gelatik-Bed-and-Breakfast/Kuta/294146?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1101lwzXIiZi&from=2023-02-19&to=2023-02-20"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gelatik Bed & Breakfast
+          </a>
+        </strong>
+        <br></br>
+        <strong>
+          Best Hotel for Partying:{" "}
+          <a
+            href="https://www.hostelworld.com/st/hostels/p/314762/karma-backpacker/?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1100lwzEHK59"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Clandestino Hotel Canggu
+          </a>
+        </strong>
+        <br></br>
+        <strong>
+          Best Hotel for Solo Travelers:{" "}
+          <a
+            href="https://www.hostelworld.com/st/hostels/p/314762/karma-backpacker/?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1011lwAsdI4j"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Karma Backpackers
+          </a>
+        </strong>
+        <br></br>
+        <strong>
+          Best Hotel for Solo Female Travelers:{" "}
+          <a
+            href="https://www.hostelworld.com/st/hostels/p/302658/arya-wellness-female-only?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1101lwzXKHyE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Arya Wellness
+          </a>
+        </strong>
+        <br></br>
+        <strong>
+          Best Hotel for Digital Nomads:{" "}
+          <a
+            href="https://www.hostelworld.com/st/hostels/p/314010/tribal-bali/?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1101lwzXKKBN"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tribal Hotel
+          </a>
+        </strong>
+        <br></br>
+        <strong>
+          Best Overall Hotel:{" "}
+          <a
+            href="https://www.hostelworld.com/st/hostels/p/302658/arya-wellness-female-only?source=affiliate-PHG-1101lHtj&affiliate=PHG&ref_id=1101lwzXKHyE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Arya Wellness
+          </a>
+        </strong>
+        <p>
+          Want the specifics of each hostel? Here’s my breakdown of the best
+          hostels in Bali and why I love them:
+        </p>
+        <strong>Price legend (per night)</strong>
+        <br></br>
+        <div>
+          <ul className="hotelsList">
+            <li className="blogList">$ = Under 150,000 IDR</li>
+            <li className="blogList">$$ = 150,000-200,000 IDR</li>
+            <li className="blogList">$$$ = Over 200,000 IDR</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h5 className="first">1. Indraprastha Guest House</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/IndraprasthaBali.jpg"
+        />
+        <p>
+          Set right on Hanoman Street, one of the main drags in Ubud,
+          Indraprastha is a favorite among returning visitors because there are
+          no dorm rooms. The guesthouse is entirely made up of affordable
+          private rooms of varying sizes, with rooms for solo travelers,
+          couples, and groups. The rooms are spacious and have AC, and the
+          comfortable mattresses are set on sturdy wooden frames. Some have
+          balconies and en suite bathrooms as well.
+        </p>
+        <p>
+          On-site amenities include a lush tropical garden that’s attached to
+          the two-story guest house, a lovely terrace, an extra-large common
+          kitchen, and friendly staff. In addition to being in the center of it
+          all, Indraprastha is nicely positioned for the hiking trails around
+          Ubud too.
+        </p>
+        <strong>Indraprastha at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$$</li>
+          <li className="blogList">Nicely located in the center of Ubud</li>
+          <li className="blogList">Welcoming staff</li>
+          <li className="blogList">All private rooms</li>
+        </ul>
+        <p>Private rooms from 200,000 IDR.</p>
+      </div>
+      <div>
+        <h5 className="first">2. Gelatik Bed & Breakfast</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/gelatikbali.jpg"
+        />
+        <p>
+          A ten-minute walk to the beach in Seminyak, just north of Kuta, and
+          about a 20-minute drive from the airport on the west coast of Bali,
+          Gelatik is a comfortable and affordable spot that offers complimentary
+          breakfast, and their bar sells fresh-squeezed juice, local beer, and
+          tasty local eats. The staff can help you book all kinds of tours and
+          activities too.
+        </p>
+        <p>
+          The B&B has two main dorm rooms: one that’s mixed and one female-only.
+          The wooden bunk beds are sturdy and, most importantly, very
+          comfortable. There are no curtain shades, but some of the wood
+          paneling helps adds a little bit of privacy. The property also has a
+          swimming pool, a communal kitchen, and some terraces to take in the
+          fresh Bali air.
+        </p>
+        <strong>Gelatik Bed & Breakfast at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$</li>
+          <li className="blogList">Nice location close to the beach</li>
+          <li className="blogList">Complimentary breakfast</li>
+          <li className="blogList">Comfortable bunk beds</li>
+        </ul>
+        <p>Beds from 120,000 IDR per night.</p>
+      </div>
+      <div>
+        <h5 className="first">3. Karma Backpackers</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/karmabackpackerbali.jpg"
+        />
+        <p>
+          Located in Uluwatu, in the southern tip of Bali on the Bukit
+          Peninsula, Karma Backpackers is loaded with great perks: an airport
+          shuttle to and from the hostel, a scooter rental service, boat
+          tickets, and a free communal dinner for guests every night (which is a
+          great way to meet other travelers). The hostel is also located just a
+          short walk to and from the beach.
+        </p>
+        <p>
+          In the garden, there’s a small pool to soak in and the rooftop bar is
+          a lively place to hang out and drink and play billiards with other
+          backpackers. The spacious dorm rooms mostly only have four beds per
+          room, and each person gets two lockers. Each bed has its own curtain
+          for privacy. There are also large private rooms with comfy double
+          beds.
+        </p>
+        <strong>Karma Backpackers at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$$$</li>
+          <li className="blogList">Located in a fun area near the beach</li>
+          <li className="blogList">Convivial rooftop common space and bar</li>
+          <li className="blogList">Complimentary dinner every night</li>
+        </ul>
+        <p>Beds from 256,000 IDR per night, private rooms from 600,000 IDR.</p>
+      </div>
+      <div>
+        <h5 className="first">4. Clandestino Hostel Canggu</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/clandestino.jpeg"
+        />
+        <p>
+          Clandestino, located north of the airport in Canggu, has a handful of
+          mixed sleeping spaces on offer and all the dorm beds come with
+          lockers, lamps, electrical outlets to charge your phone, and curtains
+          for privacy. (Pro tip: book a bed in the six-person dorm: it’s the
+          only room that has an en suite bathroom).
+        </p>
+        <p>
+          There’s a second-floor restaurant/bar that always seems to be
+          bustling, as well as a pool complete with a swim-up bar for those days
+          when you just want to relax by the pool. If you’re looking for a
+          hostel with a strong party vibe, this is the place to stay. The staff
+          are really friendly and can also help you plan your trip and book
+          excursions around the island.
+        </p>
+        <strong>Clandestino Hostel Canggu at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$$</li>
+          <li className="blogList">Pool with a swim-up bar</li>
+          <li className="blogList">Lively restaurant and bar</li>
+          <li className="blogList">Friendly and welcoming staff</li>
+        </ul>
+        <p>Beds from 165,000 IDR per night.</p>
+      </div>
+      <div>
+        <h5 className="first">5. Arya Wellness</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2023/aryawellnessbali.jpg"
+        />
+        <p>
+          Aimed mostly (but not exclusively) at female travelers, this boutique
+          wellness-focused hostel in Ubud is slightly more expensive than other
+          hostels in Bali, but it’s well worth the splurge, given the unique
+          perks here. The property has a swimming pool and offers yoga, Pilates,
+          fitness, and meditation classes. There are also movie nights and
+          cooking classes. The in-house eatery serves only vegetarian and vegan
+          dishes and breakfast is included with your stay.
+        </p>
+        <p>
+          The rooms consist of a few four-person shared sleeping quarters and
+          the beds are bigger than your usual hostel mattress. They all have
+          balconies, air conditioning, and individual lockers to ensure your
+          things are safe and secure.
+        </p>
+        <strong>Arya Wellness at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$$$</li>
+          <li className="blogList">Excellent location in the center of Ubud</li>
+          <li className="blogList">Great complimentary breakfast</li>
+          <li className="blogList">
+            Meditation, yoga, and other classes daily
+          </li>
+        </ul>
+        <p>
+          Beds from 400,000 IDR per night, private rooms from 1,200,000 IDR.
+        </p>
+      </div>
+      <div>
+        <h5 className="first">6. Tribal Hotel</h5>
+        <img
+          className="blogImage"
+          src="https://media.nomadicmatt.com/2022/tribalbali1.jpeg"
+        />
+        <p>
+          Created by fellow budget backpacker Will Hatton, Tribal is a brand-new
+          hotel, the first in Bali specifically designed for digital nomads.
+          There’s a 24/7 coworking space with super-fast Wi-Fi, tons of desks,
+          sofas, and beanbags, as well as private booths for calls and lots of
+          power outlets.When you’re done working, chill out in one of the many
+          common areas (including a huge multi-level pool), enjoy the healthy
+          and varied menu at the cafe, or grab a drink at the
+          backpacker-friendly happy hour.
+        </p>
+        <p>
+          Dorms have modern pod-style beds that have individual charging outlets
+          and reading lamps. There are also stairs to the upper bunks rather
+          than the usual noisy bunk bed ladders, and rain showers in the huge
+          shared bathrooms. The guests that Tribal attracts are a laidback,
+          young professional crowd, meaning that everyone is quiet and
+          respectful at night — you’ll get a great night’s sleep here!
+        </p>
+        <strong>Tribal at a glance:</strong>
+        <br></br>
+        <ul className="hotelsList">
+          <li className="blogList">$$</li>
+          <li className="blogList">
+            Designed for digital nomads with coworking areas and fast Wi-Fi
+          </li>
+          <li className="blogList">Modern pod-style beds</li>
+          <li className="blogList">
+            Many common areas, including a giant swimming pool
+          </li>
+        </ul>
+        <p>
+          Beds from 200,000 IDR per night, private rooms from 750,000 IDR per
+          night.
+        </p>
+      </div>
+      <p style={{ marginTop: "35px" }}>
+        Bali is a large island, so picking the right hostel based on what you
+        want to see and do is important. Fortunately, there are a ton of amazing
+        options in Bali, ensuring that you’ll have a fun and affordable visit to
+        this iconic destination.
+      </p>
+    </div>
+  );
+};
+
+export default BaliBlog;
